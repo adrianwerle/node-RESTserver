2ae5881d4b9ac7feed062e6bcbf5a6ab01cb7457
Adrian Werle <adrianwerle@live.com.ar>
2018-06-19 11:52:42 -0300

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use(require('./routes/usuario'));
+
+
+
+mongoose.connect(process.env.URLDB, (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+
+});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
